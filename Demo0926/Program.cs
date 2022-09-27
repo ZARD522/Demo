0c888a31ce1456c6ea1182fac5652bb887f90dea
ZARD <2422177510@qq.com>
2022-09-27 21:03:41 +0800

    0927

--- Demo0926/Program.cs
+++ Demo0926/Program.cs
@@ -10,6 +10,44 @@ namespace Demo0926
     {
         static void Main(string[] args)
         {
+            //数组的三种命名方式
+            int[] a = new int[10];
+            int[] b = {1,2,10};
+            int[] c = new int[] {1,9,5,2,4,7};
+            //向数组a添加数据
+            for (int i = 0; i < 10; i++)
+            {
+                //a[i] = i+1;
+                //输入数据
+                a[i]=Convert.ToInt32(Console.ReadLine());
+            }
+
+
+            //循环遍历数组a中的元素
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.WriteLine("a[{0}]={1}",i,a[i]);
+            }
+
+            //通foreach循环遍历数组a中的元素
+            foreach (int temp in a)
+            {
+                Console.Write(temp);
+            }
+
+
+            Console.ReadKey();
+
         }
+
+        public void Swap(int a,int b)
+        {
+            int min = a;
+            if (a > b)
+            {
+                min = b;
+            }
+        }
+
     }
 }
---
+++ Order/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Order
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //输入数据
+            string str = Console.ReadLine();//输入一串数字以空格符隔开
+            string[] strArray = str.Split(' ');//,用字符串数组去存储这些数字
+            int[] intArray = new int[strArray.Length];//intArray数组的长度和strArray长度相一致
+            for (int i = 0; i < strArray.Length; i++)
+            {
+                int number = Convert.ToInt32(strArray[i]);//字符型转换成整形
+                intArray[i] = number;
+            }
+
+            // int[] intArray = new int[] { 22, 33, 11, 23, 12, 45, 76, 34 };
+            BubbleSort(intArray);
+            foreach(int temp in intArray)
+            {
+                Console.Write(temp + " ");
+            }
+
+            Console.ReadKey();
+
+        }
+
+
+        static void BubbleSort(int[] intArray)
+        {
+            
+            //冒泡排序
+            for (int i = 0; i < intArray.Length - 1; i++)//进行length-1轮比较
+            {
+                //进行比较
+                for (int j = 0; j < intArray.Length - 1 - i; j++)
+                {
+                    if (intArray[j] > intArray[j + 1])//前面的数比后面的数字大就进行交换
+                    {
+                        int temp = intArray[j];
+                        intArray[j] = intArray[j + 1];
+                        intArray[j + 1] = temp;
+                    }
+
+                }
+
+            }
+        }
+
+
+    }
+}
